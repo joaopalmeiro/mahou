f122a845879c3ab6ec505e1509c9099e6cc0d003
João Palmeiro <joaommpalmeiro@gmail.com>
2020-08-30 16:51:52 +0100

    Added: First arguments.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # mahou
 
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+
 A package full of tricks... sorry, full of IPython magic commands.
 
 ## Commands
@@ -13,6 +15,7 @@ A package full of tricks... sorry, full of IPython magic commands.
 ## Documentation
 
 - Black:
+  - [CLI options](https://github.com/psf/black/blob/master/README.md#command-line-options).
   - [`format_str()`](https://github.com/psf/black/blob/master/src/black/__init__.py#L943) function.
   - [`Mode`](https://github.com/psf/black/blob/master/src/black/__init__.py#L240) class.
   - [`TargetVersion`](https://github.com/psf/black/blob/master/src/black/__init__.py#L169) enum class.
--- mahou/formatter.py
+++ mahou/formatter.py
@@ -1,13 +1,36 @@
-from black import FileMode, format_str
+from black import DEFAULT_LINE_LENGTH, FileMode, format_str
 from IPython.core import magic_arguments
+from IPython.core.debugger import set_trace
 from IPython.core.magic import Magics, cell_magic, magics_class
 
 
 @magics_class
 class Formatter(Magics):
+    @magic_arguments.magic_arguments()
+    @magic_arguments.argument(
+        "-l",
+        "--line-length",
+        type=int,
+        default=DEFAULT_LINE_LENGTH,
+        help="Number of characters allowed per line.",
+        dest="line_length",
+    )
+    @magic_arguments.argument(
+        "-S",
+        "--skip-string-normalization",
+        action="store_true",
+        help="Don't normalize string quote marks and prefixes.",
+        dest="string_normalization",
+    )
     @cell_magic
     def black(self, line, cell):
-        mode = FileMode()
+        args = magic_arguments.parse_argstring(self.black, line)
+
+        # More info about the `--target-version` option: https://github.com/psf/black/issues/751
+        mode = FileMode(
+            line_length=args.line_length,
+            string_normalization=not args.string_normalization,
+        )
 
         formatted_cell = format_str(cell, mode=mode)
         formatted_cell = (
